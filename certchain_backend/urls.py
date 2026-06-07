b7c1fd7cd4714a34e9262b8a4dbd00c4cad217fd
DanSiro <sirodaniel48@gmail.com>
2026-06-07 13:18:52 +0300

    fix: connect api routes to main project urls2

--- certchain_backend/urls.py
+++ certchain_backend/urls.py
@@ -24,8 +24,9 @@ from api.views import ProcessCertificateView, CertificateMetadataView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/', include('api.urls')), 
+     
     
     # Endpoint to refresh an expired token
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
