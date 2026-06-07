34c59a65734d150785c23db5c85b055c8083d0a9
DanSiro <sirodaniel48@gmail.com>
2026-06-07 13:22:28 +0300

    fix: add missing api urls file

---
+++ api/urls.py
@@ -0,0 +1,11 @@
+# api/urls.py
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import ProcessCertificateView, CertificateMetadataView
+
+urlpatterns = [
+    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('certificates/process/', ProcessCertificateView.as_view(), name='process_certificate'),
+    path('certificates/<str:document_hash>/metadata/', CertificateMetadataView.as_view(), name='certificate_metadata'),
+]
